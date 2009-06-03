6655a6554a0fc8ab9ff33d9a1f550229df378f9c
Christopher Brannon <cmbrannon@cox.net>
2009-06-03 14:40:31 -0500

    Miscellaneous changes to i18n.h.
    Renamed the messages array to english_messages.  It's an array of strings.
    english_strings is an array of char **, in this case an array of arrays
    of strings.
    i18n is of type char ***, and it is initially equivalent to
    english_strings.

--- src/i18n.h
+++ src/i18n.h
@@ -1,25 +1,31 @@
+#ifndef I18N_H
+#define I18N_H
 /* Internationalization declarations */
 
 #define CHARACTERS 0
 #define MSGS 1
 #define COLORS 2
 #define HELP 3
+#define EDGENAMES 4
 
 #define BLANK 0
-#define IAM_ALIVE 2
-#define YOU_KILLED_SPEAKUP 3
-#define HEY_THATS_BETTER 4
-#define YOU_TURNED_ME_OFF 5
+#define IAM_ALIVE 1
+#define YOU_KILLED_SPEAKUP 2
+#define HEY_THATS_BETTER 3
+#define YOU_TURNED_ME_OFF 4
 
 
 /* I'll start it here to keep the indices and messages in sync. */
 
-char *messages = {
+char *english_messages [] ={
 	"blank",
 	"I'm aLive!",
 	"You killed speakup!",
 	"hey. That's better!",
 	"You turned me off!",
 
-}
+};
 
+char **english_strings [] = {NULL, english_messages, NULL, NULL, NULL};
+char ***i18n = english_strings;
+#endif
